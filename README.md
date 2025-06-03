64c75f77b9acd24c5f030677b2a278af74c11b70
Eduardo Leffa <eduardo.leffa@visto.global>
2025-06-03 13:56:39 -0300

    types

---
+++ README.md
@@ -0,0 +1,30 @@
+# Types
+
+## number
+
+**Unsigned integer**
+Uint8, Uint16, Uint32, Uint64 - only numbers positives
+
+**integer**
+
+- int8, int16, int32, int64 - positives and negatives
+
+**Curiosity**:
+Uint8 -> byte
+Int32 -> rune
+
+**float**
+
+float32, float64
+
+**string**
+
+"" or ``
+
+**boolean**
+true or false
+
+**logic operators**
+&& (and)
+|| (or)
+! (negative)
--- first-class.go
+++ first-class.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-		fmt.Println("Hello, World!")
+		fmt.Println("Type: number")
+		fmt.Println("1 + 1 =", 1 + 1)
+		fmt.Println("Type: float")
+		fmt.Println("2.5 + 2.5 =", 2.5 + 2.5)
+		fmt.Println("Type: string")
+		fmt.Println("Hello, " + "World!")
+		fmt.Println(len("Hello World!"))
+		fmt.Println("Hello"[2])
+		fmt.Println("Type: boolean")
+		fmt.Println("true && true", true && true)
+		fmt.Println("true && false", true && false)
+		fmt.Println("true || true", true || true)
+		fmt.Println("true || false", true || false)
+		fmt.Println("!true", !true)
 }
