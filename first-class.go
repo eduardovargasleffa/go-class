af2282a8bed4215a02f9df11544cce0632005d3d
Eduardo Leffa <eduardo.leffa@visto.global>
2025-06-03 14:04:20 -0300

    vars and tests

--- first-class.go
+++ first-class.go
@@ -3,18 +3,25 @@ package main
 import "fmt"
 
 func main() {
-		fmt.Println("Type: number")
-		fmt.Println("1 + 1 =", 1 + 1)
-		fmt.Println("Type: float")
-		fmt.Println("2.5 + 2.5 =", 2.5 + 2.5)
-		fmt.Println("Type: string")
-		fmt.Println("Hello, " + "World!")
-		fmt.Println(len("Hello World!"))
-		fmt.Println("Hello"[2])
-		fmt.Println("Type: boolean")
-		fmt.Println("true && true", true && true)
-		fmt.Println("true && false", true && false)
-		fmt.Println("true || true", true || true)
-		fmt.Println("true || false", true || false)
-		fmt.Println("!true", !true)
+		// fmt.Println("Type: number")
+		// fmt.Println("1 + 1 =", 1 + 1)
+		// fmt.Println("Type: float")
+		// fmt.Println("2.5 + 2.5 =", 2.5 + 2.5)
+		// fmt.Println("Type: string")
+		// fmt.Println("Hello, " + "World!")
+		// fmt.Println(len("Hello World!"))
+		// fmt.Println("Hello"[2])
+		// fmt.Println("Type: boolean")
+		// fmt.Println("true && true", true && true)
+		// fmt.Println("true && false", true && false)
+		// fmt.Println("true || true", true || true)
+		// fmt.Println("true || false", true || false)
+		// fmt.Println("!true", !true)
+
+		var name string = "Eduardo Leffa"
+		var age int = 31
+		var version float32 = 1.24
+		fmt.Println("Hello, mrs.", name, " your age is", age)
+		fmt.Println("This application is in version ", version)
+
 }
